b58da65b1f787df3eeab4fa6babf6e0c4eaf87a6
Ryan G <exogenesis@protonmail.com>
2025-09-06 22:16:17 +1000

    update exports

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 // NOTE do not export the presets through here, export them through
 // the package.json directly!
 
-// export * from "./components/Layout";
-// export * from "./components/Layout/styled";
-// export * from "./components/Combobox";
-//
-// export { useResizeKeybindings } from "./components/Layout/utilities/useResizeKeybindings";
-// export { VirtualizedDataTable } from "./components/DataTables/VirtualizedDatatable";
-// export { VirtualList } from "./components/VirtualList";
-// export { SingleCombobox } from "./components/Combobox/SingleCombobox";
-// export { VirtualPhotoGrid } from "./components/VirtualPhotoGrid";
-// export { Progress } from "./components/Progress";
-// export { Badge } from "./components/Badge";
+export * from "./components/Layout";
+export * from "./components/Layout/styled";
+export * from "./components/Combobox";
+
+export { useResizeKeybindings } from "./components/Layout/utilities/useResizeKeybindings";
+export { VirtualizedDataTable } from "./components/DataTables/VirtualizedDatatable";
+export { VirtualList } from "./components/VirtualList";
+export { SingleCombobox } from "./components/Combobox/SingleCombobox";
+export { VirtualPhotoGrid } from "./components/VirtualPhotoGrid";
+export { Progress } from "./components/Progress";
+export { Badge } from "./components/Badge";
 export { Button } from "./components/Button";
+export { Kbd } from "./components/Kbd/Kbd";
